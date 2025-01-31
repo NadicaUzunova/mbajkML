ada583dc391c975ec775ca75749074cf7e487ad4
Nadica <nadica.uzunova@student.um.si>
2025-01-31 13:27:30 +0100

    update deps

--- src/models/predict_model_mlflow.py
+++ src/models/predict_model_mlflow.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import mlflow
 import mlflow.tensorflow
-from tensorflow.keras.models import load_model
+from keras.models import load_model
 from sklearn.metrics import mean_absolute_error, mean_squared_error, explained_variance_score
 
 def normalize_string(input_str: str) -> str:
--- src/models/train_model_mlflow.py
+++ src/models/train_model_mlflow.py
@@ -4,8 +4,8 @@ import numpy as np
 import mlflow
 import mlflow.tensorflow
 import tensorflow as tf
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, LSTM
+from keras.models import Sequential
+from keras.layers import Dense, LSTM
 import unicodedata
 
 def create_model(input_shape):
